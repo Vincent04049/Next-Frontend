ceec1893fb9ca3dbc6f06df1c1797b3a735fa938
Vincent04049 <Vincent04049@outlook.com>
2023-06-17 09:33:04 +0500

    navbar set up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,9 @@ import CustomButton  from './CustomButton';
 
 const Navbar = () => {
   return (
-	<header className='w-full absolute z-10'></header>
+	<header className='w-full absolute z-10'>
+		<nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 '></nav>
+	</header>
   )
 }
 
