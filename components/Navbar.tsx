c482feba6549d6a7fd40e70ee6c841a95a6a7e4e
Vincent04049 <Vincent04049@outlook.com>
2023-06-17 09:30:32 +0500

    navbar set up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import CustomButton  from './CustomButton';
 
 const Navbar = () => {
   return (
-	<div>Navbar</div>
+	<header className='w-full absolute z-10'></header>
   )
 }
 
