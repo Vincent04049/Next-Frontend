6df8eb93cb0842515a91f304c4bb73064343a854
Vincent04049 <Vincent04049@outlook.com>
2023-06-17 09:26:03 +0500

    navbar set up

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+	<div>Navbar</div>
+  )
+}
+
+export default Navbar
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
 import CustomButton from "./CustomButton";
 import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from './Navbar';
 
 
 
-export {CustomButton,Hero}
+export { CustomButton, Hero, Footer, Navbar }
